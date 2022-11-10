d81661dda57f2614feca45003fd7daa7b137f24a
jecolombo <joaquincolombo494@gmail.com>
2022-11-10 19:55:40 +0100

    Inicializacion
    Se establecieron los componentes.
    Se realizo el ruteo.
    Se avanzo con el home y el navbar.
    FALTA CONFIGURAR LOS COLORES DE BOOTSTRAP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
 
+import { RouterModule } from '@angular/router';
+import { AboutassComponent } from './components/aboutass/aboutass.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BannerComponent,
+    AboutassComponent,
+    ProductsComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'aboutass',component:AboutassComponent},
+      {path:'products',component:ProductsComponent},
+      {path:'contacts',component:ContactsComponent},
+      {path:'**',redirectTo:'/',pathMatch:'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/aboutass/aboutass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutass',
+  templateUrl: './aboutass.component.html',
+  styleUrls: ['./aboutass.component.css']
+})
+export class AboutassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
